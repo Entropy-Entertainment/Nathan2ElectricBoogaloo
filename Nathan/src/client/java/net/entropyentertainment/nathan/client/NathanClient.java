718779454b03d6ee2dcea7db28daaf3ff8cead58
null/ <null.ptr.exc@outlook.com>
2025-02-18 15:21:28 +0100

    Initial project setup

---
+++ Nathan/src/client/java/net/entropyentertainment/nathan/client/NathanClient.java
@@ -0,0 +1,10 @@
+package net.entropyentertainment.nathan.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NathanClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ Nathan/src/main/java/net/entropyentertainment/nathan/Nathan.java
@@ -0,0 +1,10 @@
+package net.entropyentertainment.nathan;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Nathan implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
