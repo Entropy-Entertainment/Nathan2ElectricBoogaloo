262a9b881ee92cbbe1db5cf995c5dbeb647fc66b
null/ <null.ptr.exc@outlook.com>
2025-02-18 21:23:46 +0100

    Added InitializerHelper (#2)
    Added InitializerHelper and a lot of documentation for what it does. It should make adding items and itemgroups a *lot* easier. This class will expand in the future

--- src/main/java/net/entropyentertainment/nathan/Nathan.java
+++ src/main/java/net/entropyentertainment/nathan/Nathan.java
@@ -1,14 +1,21 @@
 package net.entropyentertainment.nathan;
 
+import net.entropyentertainment.nathan.init.ModEssentials;
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Nathan implements ModInitializer {
+public class Nathan extends ModEssentials implements ModInitializer {
 
-    public static final Logger LOGGER = LogManager.getLogger("Nathan");
+    public Nathan() {
+        MOD_ID = "nathan";
+        LOGGER.info("Test meow test");
+    }
+
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
+        LOGGER.info("Test succes");
     }
 }
